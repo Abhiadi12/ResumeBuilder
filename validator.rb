75c6e5c0785edbb6840324b20b723ebbb740666e
abhi_sek12 <57040284+Abhiadi12@users.noreply.github.com>
2020-02-17 12:44:01 +0530

    Update validator.rb
    Make changes as per the guide.

--- validator.rb
+++ validator.rb
@@ -1,28 +1,15 @@
-
-	module Validator
-
+module Validator
+		NAME_REGEX=/^[a-zA-Z]{3,16}+$/
+		AGE_REGEX=/^(([1-9]){1}([0-9]){1})$/
 		def is_correct_name?(str)
-			if str.match(/^[a-zA-Z]{3,16}+$/)==nil
-				return false
-			else
-				return true
-			end
+			!(str.match(NAME_REGEX).nil?)
 		end
 
 		def is_correct_age?(age)
-			if age>=18
-				return true
-			else
-				return false
-			end
+			!(age.match(AGE_REGEX).nil?) && age.to_i >=18
 		end
 
 		def is_correct_gender?(gender)
-			if gender.upcase=="MALE" || gender.upcase=="FEMALE"
-				return true
-			else
-				return false
-			end
+			gender.upcase=="MALE" || gender.upcase=="FEMALE" 
 		end
-
-	end
+end
