e77cd4ad19fcf63cd02818385e8b7b884057b776
Abhiadi12 <abhisekmishra172@gmail.com>
2021-05-30 18:37:27 +0530

    max limit added

--- resume.rb
+++ resume.rb
@@ -1,20 +1,47 @@
 class Resume
 	#require "csv" load in runner method
 	include Validator
-	attr_accessor:first_name,:last_name,:age,:gender
+	attr_accessor:first_name,:last_name,:age,:gender,:limit
 	
 	def self.build 
 		object = self.new
+		object.limit = 0;
 		loop do
 			puts " Do You Want To Create A Resume[Y-N]"
 			response = gets.chomp.downcase
-			break if response == "n"
+			break if response != "y"
 			object.first_name = object.looping_entry("first_name")
+			if object.limit_reach
+				puts "limit reach"
+				break
+			end
 			object.last_name = object.looping_entry("last_name")
+			if object.limit_reach
+				puts "limit reach"
+				break
+			end
+
+
 			object.age = object.looping_entry("age")
+			if object.limit_reach
+				puts "limit reach"
+				break
+			end
+
+
 			object.gender = object.looping_entry("gender")
+			if object.limit_reach
+				puts "limit reach"
+				break
+			end
+
+
 			object.load_to_csv
 			end
+	end
+
+	def limit_reach
+		self.limit > 3
 	end
 
 	def load_to_csv
@@ -29,18 +56,35 @@ class Resume
 	def looping_entry(entry_message)
 		@entry
 		loop do
+			break if self.limit > 2
 			puts " Enter your #{entry_message}"
 			@entry = gets.chomp
 			if entry_message == "first_name" || entry_message == "last_name"
 				break if self.is_correct_name?(@entry)
 			elsif entry_message == "age"
-				break if self.is_correct_age?(@entry.to_i)
+				break if self.is_correct_age?(@entry)
 			else
 				break if self.is_correct_gender?(@entry)
 			end
 			puts "Wrong Data Entry:::Try Again"
+			
+			self.limit = self.limit + 1
+
 		end
 		return @entry
 	end
-
 end
+
+=begin
+operations.each do |input,validate|
+          begin 
+              user.send(input)
+              user.send(validate)
+          rescue => e
+            puts e
+            retry
+          else
+            next
+          end
+      end
+=end
